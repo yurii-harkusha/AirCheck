c014f7cdcecd72bff70d9c6776fd464a8ebfe81f
Yurii Harkusha <yurii.harkusha@gmail.com>
2020-04-09 20:25:41 +0300

    Some changes for command on button

--- AirCheck.Core/ViewModels/MainPageViewModel.cs
+++ AirCheck.Core/ViewModels/MainPageViewModel.cs
@@ -3,21 +3,22 @@ using System.Collections.Generic;
 using System.Text;
 using Xamarin.Forms;
 using System.Windows.Input;
+using System.Threading.Tasks;
 
 namespace AirCheck.Core.ViewModels
 {
     public class MainPageViewModel: BaseViewModel
     {
-        public Command GetAirInfoByIpCommand = new Command(GetAirInfoByIpCommandExecute);
-
-        private static void GetAirInfoByIpCommandExecute(object obj)
-        {
-            
-        }
-
         public MainPageViewModel()
         {
+            GetAirInfoByIpCommand = new Command(async () => await GetAirInfoByIpCommandExecute());
+        }
 
+        public Command GetAirInfoByIpCommand { get; set; }
+
+        private static Task GetAirInfoByIpCommandExecute()
+        {
+            return Task.Delay(100);
         }
     }
 }
